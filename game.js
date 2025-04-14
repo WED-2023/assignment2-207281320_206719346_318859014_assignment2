d294d56801ece9dddda092a6c213e7b04ba95333
Lyubansky <dlyubansky@gmail.com>
2025-04-14 16:14:18 +0300

    initial

---
+++ game.js
@@ -0,0 +1,12 @@
+document.addEventListener('keydown', function(event) {
+    if(event.key === 'ArrowLeft') {
+        // Move player spaceship left
+    }
+    if(event.key === 'ArrowRight') {
+        // Move player spaceship right
+    }
+});
+
+function shoot() {
+    // Implement shooting function for the player
+}
